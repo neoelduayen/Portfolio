255aad6c476bd029f5667dde5692beccbcdc6ea6
neoelduayen <nelduayen@univ-pau.fr>
2025-03-11 12:08:51 +0100

    Début de la création de la page d'accueil

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  searchQuery: string = '';
+  
+  constructor(private router: Router) {}
+  
+  searchProjects(): void {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/projets'], { 
+        queryParams: { search: this.searchQuery } 
+      });
+    }
+  }
+  
+  navigateToContact(): void {
+    this.router.navigate(['/contact']);
+  }
+  
+  navigateToProjects(): void {
+    this.router.navigate(['/projets']);
+  }
 }
