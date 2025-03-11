d9880c29c22219226dff37bdf141a39db3e45c80
neoelduayen <nelduayen@univ-pau.fr>
2025-03-11 10:34:34 +0100

    Création du header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  animations: [
+    trigger('slideInOut', [
+      state('closed', style({
+        height: '0px',
+        opacity: 0,
+        visibility: 'hidden'
+      })),
+      state('open', style({
+        height: '*',
+        opacity: 1,
+        visibility: 'visible'
+      })),
+      transition('closed => open', [
+        animate('300ms ease-in-out')
+      ]),
+      transition('open => closed', [
+        animate('300ms ease-in-out')
+      ])
+    ])
+  ]
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
+  }
 }
